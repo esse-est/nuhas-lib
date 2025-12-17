c356b4906a6692d6677d5717ba913436fe60d552
esse-est <nokor9876@gmail.com>
2025-12-17 18:57:07 +0000

    bw+arith functions. truthtable debugging

--- nlm.py
+++ nlm.py
@@ -1,66 +1,43 @@
 
+MEM={}
 
-class DataObject():
-    def __init__(self,data:bytes):
-        self.data=data
-    def __call__(self):
-        return(self.data)
-    def que(self,i:int):
-        return(int(str(self.data)[2:][i]))
+# bw
+def BAND(foo:chr,bar:chr):
+    """Bitwise AND"""
+    return(chr(ord(foo) & ord(bar)))
+def BXOR(foo:chr,bar:chr):
+    """Bitwise XOR"""
+    return(chr(ord(foo) ^ ord(bar)))
+def BNOT(foo:chr):
+    """Bitwise NOT"""
+    return(chr(~ord(foo)))
+def BORR(foo:chr,bar:chr):
+    """Bitwise OR"""
+    return(chr(ord(foo) | ord(bar)))
+def BSSL(foo:chr):
+    """Bitwise Static Shift Left"""
+    return(chr(ord(foo)<<1))
+def BSSR(foo:chr):
+    """Bitwise Static Shift Right"""
+    return(chr(ord(foo)>>1))
+def BBIC(foo:chr,bar:chr):
+    """Bitwise Bit Clear"""
+    return (
+        chr(ord(foo) & ~ord(bar))
+    )
 
+# arith
+def AADD(foo:chr,bar:chr):
+    """Arith Addition"""
+    return(chr(ord(foo) + ord(bar)))
+def AMIN(foo:chr,bar:chr):
+    """Arith Subtraction"""
+    return(chr(ord(foo) - ord(bar)))
+def AMUL(foo:chr,bar:chr):
+    """Arith Multiplication"""
+    return(chr(ord(foo) * ord(bar)))
 
-# logic:
-def AND_B(ADD1:DataObject,ADD2:DataObject):
-    constructor=""
-    for i in range(len(str(ADD2.data)[2:-1])):
-        if ADD1.que(i) == 1 and ADD2.que(i) == 1:
-            constructor=f"{constructor}{1}"
-        else:
-            constructor=f"{constructor}0"
-    return(f"b'{constructor}'")
+# manip
 
-def OR_B(ADD1:DataObject,ADD2:DataObject):
-    constructor=""
-    for i in range(len(str(ADD2.data)[2:-1])):
-        if ADD1.que(i) == 1 or ADD2.que(i) == 1:
-            constructor=f"{constructor}{1}"
-        else:
-            constructor=f"{constructor}0"
-    return(f"b'{constructor}'")
+# comp/read
 
-def XOR_B(ADD1:DataObject,ADD2:DataObject):
-    constructor=""
-    for i in range(len(str(ADD2.data)[2:-1])):
-        if (ADD1.que(i) + ADD2.que(i)) == 1:
-            constructor=f"{constructor}{1}"
-        else:
-            constructor=f"{constructor}0"
-    return(f"b'{constructor}'")
-
-# assignment:
-def MOV_B():
-    pass
-
-def LSL_B():
-    pass
-
-def LSR_B():
-    pass
-
-def ASR_B():
-    pass
-
-def ROR():
-    pass
-
-comma=DataObject(b"0")
-dash=DataObject(b"1")
-
-
-for i in [AND_B,OR_B,XOR_B]:
-    print(f"{i}")
-    
-    print(i(comma,comma))
-    print(i(comma,dash))
-    print(i(dash,comma))
-    print(i(dash,dash))
---
+++ truthtable_js.py
@@ -0,0 +1,41 @@
+from nlm import *
+import json
+
+
+function_translation_key={
+    "BAND":BAND,
+    "BXOR":BXOR,
+    "BNOT":BNOT,
+    "BORR":BORR,
+    "BSSL":BSSL,
+    "BSSR":BSSR,
+    "BBIC":BBIC,
+    "AADD":AADD,
+    "AMIN":AMIN,
+    "AMUL":AMUL
+}
+
+#loaded truth table
+def exec_truth_table(path):
+    """Pulls truth table from the json file in path, compares it to values generated from nlm.py. debugging system"""
+    selection=str
+    with open(path,"r") as f:
+        global ltt
+        ltt=(json.load(f))
+    
+    while selection not in ltt:
+        for i in ltt:
+            print(i)
+        selection=input("Truth Table to run: ")
+    
+    outs=[]
+    for i in ltt[selection][1]:
+        outs.append(function_translation_key[i](chr(ltt[selection][0][0]),chr(ltt[selection][0][1])))
+    for i in range(len(outs)):
+        print(f"{ord(outs[i])} | {ltt[selection][2][i]} > ",end="")
+        if outs[i] == chr(ltt[selection][2][i]):
+            print(True)
+        else:
+            print(False)
+
+exec_truth_table("default_tt.json")
